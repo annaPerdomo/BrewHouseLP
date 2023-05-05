db45f99ae30737b66cd5f7c7f233f680803ef5b2
Daniel Mendez <awilddanoru@gmail.com>
2023-05-05 13:17:46 -0700

    refactor: Move address and hours to components for ease of readability

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
 
-import AboutUs from '../src/components/AboutUs';
-import Copyright from '../src/components/Copyright';
-import FeaturedItems from '../src/components/FeaturedItems';
-import Menu from '../src/components/Menu';
-import useDeviceSize from '../src/hooks/useDeviceSize';
+import AboutUs from "../src/components/AboutUs";
+import Address from "../src/components/Address";
+import Copyright from "../src/components/Copyright";
+import FeaturedItems from "../src/components/FeaturedItems";
+import Hours from "../src/components/Hours";
+import Menu from "../src/components/Menu";
+import useDeviceSize from "../src/hooks/useDeviceSize";
 
 export default function Home(): React.ReactElement<any> {
   const { width } = useDeviceSize();
@@ -20,9 +19,9 @@ export default function Home(): React.ReactElement<any> {
     <>
       <Box
         sx={{
-          pt: '1.5%',
-          textAlign: 'center',
-          width: '100%',
+          pt: "1.5%",
+          textAlign: "center",
+          width: "100%",
         }}
       >
         <img
@@ -37,8 +36,8 @@ export default function Home(): React.ReactElement<any> {
 
       <Box
         sx={{
-          backgroundColor: 'secondary.main',
-          paddingBottom: '3%',
+          backgroundColor: "secondary.main",
+          paddingBottom: "3%",
         }}
       >
         <br />
@@ -46,27 +45,27 @@ export default function Home(): React.ReactElement<any> {
           variant="h4"
           color="black"
           gutterBottom
-          sx={{ textAlign: 'center', mb: '2%' }}
+          sx={{ textAlign: "center", mb: "2%" }}
         >
           Featured Items
         </Typography>
         <FeaturedItems />
       </Box>
 
-      <Container maxWidth="lg" sx={{ overflow: 'hidden' }}>
+      <Container maxWidth="lg" sx={{ overflow: "hidden" }}>
         <Box
           sx={{
-            mb: '1%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
+            mb: "1%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
           <Typography
             variant="h3"
             gutterBottom
-            sx={{ textAlign: 'center', mb: '1%', mt: '2%' }}
+            sx={{ textAlign: "center", mb: "1%", mt: "2%" }}
           >
             Menu
           </Typography>
@@ -80,7 +79,7 @@ export default function Home(): React.ReactElement<any> {
         </Box>
       </Container>
 
-      <Box sx={{ backgroundColor: 'secondary.main' }}>
+      <Box sx={{ backgroundColor: "secondary.main" }}>
         <br />
         <AboutUs />
       </Box>
@@ -88,76 +87,24 @@ export default function Home(): React.ReactElement<any> {
       <Container
         maxWidth="lg"
         sx={{
-          overflow: 'hidden',
+          overflow: "hidden",
           mb: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <br />
         <br />
 
-        <Typography variant="h5">Address</Typography>
-
-        <Typography>111 S 1st St</Typography>
-        <Typography>La Puente, CA 91744</Typography>
+        <Address />
 
         <br />
         <br />
 
-        <Typography variant="h5">Hours</Typography>
+        <Hours />
 
-        <List dense sx={{ pb: 4 }}>
-          <ListItem>
-            <Grid alignItems="center" container justifyContent="space-between">
-              <Grid item>
-                <Typography
-                  sx={{ textTransform: 'capitalize', width: '200px' }}
-                >
-                  Monday - Friday
-                </Typography>
-              </Grid>
-
-              <Grid item>
-                <Typography>Closed</Typography>
-              </Grid>
-            </Grid>
-          </ListItem>
-
-          <ListItem>
-            <Grid alignItems="center" container justifyContent="space-between">
-              <Grid item>
-                <Typography
-                  sx={{ textTransform: 'capitalize', width: '200px' }}
-                >
-                  Saturday
-                </Typography>
-              </Grid>
-
-              <Grid item>
-                <Typography>8:00am - 6:00pm</Typography>
-              </Grid>
-            </Grid>
-          </ListItem>
-
-          <ListItem>
-            <Grid alignItems="center" container justifyContent="space-between">
-              <Grid item>
-                <Typography
-                  sx={{ textTransform: 'capitalize', width: '200px' }}
-                >
-                  Sunday
-                </Typography>
-              </Grid>
-
-              <Grid item>
-                <Typography>8:00am - 6:00pm</Typography>
-              </Grid>
-            </Grid>
-          </ListItem>
-        </List>
         <Copyright />
       </Container>
     </>
---
+++ src/components/Address.tsx
@@ -0,0 +1,12 @@
+import Typography from "@mui/material/Typography";
+
+export default function Address() {
+  return (
+    <>
+      <Typography variant="h5">Address</Typography>
+
+      <Typography>111 S 1st St</Typography>
+      <Typography>La Puente, CA 91744</Typography>
+    </>
+  );
+}
